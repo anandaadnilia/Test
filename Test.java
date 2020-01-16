03413bdfad876ec8e10d0a5bc622acb3a68eb138
anandaadnilia <anandaadnilia@gmail.com>
2020-01-16 08:59:17 +0700

    lala '

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 public class Test {
 	public static void main(String[] args) {
 	System.out.println("Hi World");
+	System.out.println("Hello World!!!!!!!");
+	System.out.println("HIIII!!!!");
 	}
 }
